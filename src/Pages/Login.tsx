3ac474700d8f789b20334961bc2e2c43b3c0d5ea
Gabriel mackenzie <ken@Gabriels-MacBook-Air.local>
2023-11-30 16:35:47 +0700

    Edit News

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -2,16 +2,10 @@ import React, { ChangeEvent, FormEvent, useState } from "react";
 import './RegisternLogin.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
-
-interface LoginForm {
-    email: string;
-    password: string;
-}
-
 export function Login() {
-    const [formData, setFormData] = useState<LoginForm>({
-        email: "",
-        password: "",
+    const [formData, setFormData] = useState({
+        email: '',
+        password: '',
     });
     const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
         const { name, value } = e.target;
@@ -40,12 +34,12 @@ export function Login() {
                     <div>
                     <div className="input-field">
                         <i className='fa-solid fa-envelope'></i>
-                        <input type="text" placeholder="E-mail" id="Email" value={formData.email} onChange={handleInputChange} />
+                        <input type="email" placeholder="E-mail" id="Email" value={formData.email} onChange={handleInputChange} />
                     </div>
 
                     <div className="input-field">
                         <i className='fa-solid fa-lock'></i>
-                        <input type="text" placeholder="Password" id="Password" value={formData.password} onChange={handleInputChange} />
+                        <input type="password" placeholder="Password" id="Password" value={formData.password} onChange={handleInputChange} />
                     </div>
 
                     <div className="btn-field">
