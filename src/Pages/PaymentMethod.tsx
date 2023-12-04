57b80b1caf11f9d1f2aa983cb1d96d9caa08311b
chnJia <angelangelch15@gmail.com>
2023-12-04 21:23:41 +0700

    seat success + paymentmethod success

--- src/Pages/PaymentMethod.tsx
+++ src/Pages/PaymentMethod.tsx
@@ -1,6 +1,7 @@
 import React, { ChangeEvent, FormEvent, useEffect, useState } from 'react';
 import './PaymentMethod.css';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 interface PaymentMethod {
     paymentMethodId: number;
@@ -29,16 +30,24 @@ export function PaymentMethod() {
         setSelectedPaymentMethod(e.target.value);
     };
 
+    const navigate = useNavigate()
+
     const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-        // Add your form submission logic here
+        e.preventDefault()
+
+        if (!selectedPaymentMethod) {
+            alert("Please choose a payment method")
+        }
+
+        alert("Payment Successful")
+        navigate("/myTicket")
     };
 
     const ButtonDisabled = !selectedPaymentMethod;
     const buttonColor = ButtonDisabled ? "grey" : "rgb(243, 182, 100)";
 
     console.log('Payment Methods: ', paymentMethodData);
-    
+
     paymentMethodData.forEach((paymentMethod) => {
         console.log('Payment Method Name: ', paymentMethod.paymentMethodName);
     });
@@ -51,34 +60,34 @@ export function PaymentMethod() {
             <form className="payment-form" onSubmit={handleSubmit} id="form-payment">
                 <h2>Please Choose</h2>
 
-                            <div className="payment-options">
-                                {paymentMethodData.map((paymentMethod) => (
-                                    <div key={paymentMethod.paymentMethodId}>
-                                        <div className="payment">
-                                            <input
-                                                type="radio"
-                                                name="paymentOption"
-                                                onChange={(e: ChangeEvent<HTMLInputElement>) => handleOptionChange(e)}
-                                            />
-                                            {paymentMethod.paymentMethodName === 'Gopay' && (
-                                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Gopay_logo.svg/462px-Gopay_logo.svg.png?20210531070158" alt="gopay logo" style={{ height: '25px' }}/>
-                                            )}
-                                            {paymentMethod.paymentMethodName === 'OVO' && (
-                                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/768px-Logo_ovo_purple.svg.png" alt="ovo logo"  style={{ height: '20px' }}/>
-                                            )}
-                                            {paymentMethod.paymentMethodName === 'BCA' && (
-                                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Bank_Central_Asia.svg/1199px-Bank_Central_Asia.svg.png" alt="bca logo" style={{ height: '25px' }}/>
-                                            )}
+                <div className="payment-options">
+                    {paymentMethodData.map((paymentMethod) => (
+                        <div key={paymentMethod.paymentMethodId}>
+                            <div className="payment">
+                                <input
+                                    type="radio"
+                                    name="paymentOption"
+                                    onChange={(e: ChangeEvent<HTMLInputElement>) => handleOptionChange(e)}
+                                />
+                                {paymentMethod.paymentMethodName === 'Gopay' && (
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Gopay_logo.svg/462px-Gopay_logo.svg.png?20210531070158" alt="gopay logo" style={{ height: '25px' }} />
+                                )}
+                                {paymentMethod.paymentMethodName === 'OVO' && (
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/768px-Logo_ovo_purple.svg.png" alt="ovo logo" style={{ height: '20px' }} />
+                                )}
+                                {paymentMethod.paymentMethodName === 'BCA' && (
+                                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Bank_Central_Asia.svg/1199px-Bank_Central_Asia.svg.png" alt="bca logo" style={{ height: '25px' }} />
+                                )}
 
-                                            {paymentMethod.paymentMethodName === 'Shopee-pay' && (
-                                                <img src="https://ww2.freelogovectors.net/wp-content/uploads/2023/10/shoppeepay_logo-freelogovectors.net_-640x400.png?lossy=1&ssl=1&fit=640%2C400" alt="shopee logo" style={{ height: '30px' }}/>
-                                            )}
-                                        </div>
-                                    </div>
-                                ))}
+                                {paymentMethod.paymentMethodName === 'Shopee-pay' && (
+                                    <img src="https://ww2.freelogovectors.net/wp-content/uploads/2023/10/shoppeepay_logo-freelogovectors.net_-640x400.png?lossy=1&ssl=1&fit=640%2C400" alt="shopee logo" style={{ height: '30px' }} />
+                                )}
                             </div>
+                        </div>
+                    ))}
+                </div>
                 <button id="pay-button" type="submit" disabled={ButtonDisabled} style={{ backgroundColor: buttonColor }}>
-                    Bayar
+                    Pay
                 </button>
             </form>
         </div>
